0b406ae16f811c7d801c18c4362a1750c18ac841
Nicolas MURE <nicolas.mure2019@gmail.com>
2019-10-14 16:39:24 +0200

    basic CLI parsing

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 .PHONY: shell
 shell:
-	docker-compose run --rm \
-		app \
-		/bin/sh
+	docker-compose run --rm app \
+		/bin/bash
+
+.PHONY: build-release
+build-release:
+	docker-compose run --rm app \
+		cargo build --release
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "ssc", about = "should-skip-ci")]
+struct Opt {
+    #[structopt(short = "c", long = "config", help = "Config file (YAML).")]
+    config_file: PathBuf,
+
+    #[structopt(help = "The app name for which this CI job is for.")]
+    app_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    println!("Config file is {:?}.", opt.config_file.to_str().unwrap());
+    println!("App name is {:?}.", opt.app_name);
 }

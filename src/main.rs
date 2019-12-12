ebf6958a637413eb4ca97f0dc507865a7df7f7b0
Nicolas MURE <nicolas.mure2019@gmail.com>
2019-12-12 11:01:23 +0100

    split main.rs into a lib to facilitate functional tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
+mod lib;
+
 use std::process::exit;
 
 use cli::Cli;
-use git::commits_range::CommitsRange;
-use git::commits_range::resolve_commits_range;
-use git::path_inspector::has_changes_in_paths;
-use ci::run_stop_cmd;
+use lib::should_skip_ci;
 
 fn main() {
     let cli: Cli = Cli::new();
 
-    let commits_range: CommitsRange = resolve_commits_range(
+    let status_code: i32 = should_skip_ci(
+        cli.paths(),
+        cli.cmd(),
         cli.remote(),
         cli.base_branch(),
     );
 
-    if has_changes_in_paths(&commits_range, cli.paths()) {
-        println!("Changes detected. The CI build should continue.");
-    } else {
-        println!("No changes detected. Running the stop command.");
-
-        let status_code: i32 = run_stop_cmd(cli.cmd());
-
-        exit(status_code);
-    }
+    exit(status_code);
 }
